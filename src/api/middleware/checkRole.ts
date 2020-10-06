94d29bce55df3f1b1cdf69e6449077821d7e3b23
Edy <edy.salavastru@gmail.com>
2020-10-06 11:41:50 +0300

    fix "403 when not admin"

--- src/api/middleware/checkRole.ts
+++ src/api/middleware/checkRole.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 export default (requiredRole: string) => (req: Request, res: Response, next: NextFunction): void => {
     console.log('Required role?');
     if (req.currentUser?.role !== requiredRole) {
-        res.status(401).send();
+        res.status(403).send();
     } else {
         console.log('User meet required role, going to next middleware');
         next();
